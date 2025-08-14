271360d5a6e0e5e5769f0dc5c6b07048f50dca7c
rautenfeld <christoph.rautenfeld@gmail.com>
2025-08-15 00:34:40 +0200

    updated

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Rautenfeld ORG";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Berens von Rautenfeld ORG";
+export const SITE_DESCRIPTION = "Welcome to our family's website!";
